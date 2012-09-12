9da232a72561f70153e9daf9915168e8836ce73d
Ari Russo <ari.russo@gmail.com>
2012-09-12 19:33:11 -0400

    path can just be an attr

--- lib/passifier/static_file.rb
+++ lib/passifier/static_file.rb
@@ -4,17 +4,13 @@ module Passifier
 
   class StaticFile
 
-    attr_reader :content, :name
+    attr_reader :content, :name, :path
     alias_method :filename, :name
 
     def initialize(name, path_to_file)
       @name = name
       @path = path_to_file
       @content = File.open(@path, 'rb') {|file| file.read }
-    end
-
-    def path(*a)
-      @path
     end
 
   end
