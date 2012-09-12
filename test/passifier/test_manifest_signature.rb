04a8632f4bf4e18069bae552bb928c738f991c17
Ari Russo <ari.russo@gmail.com>
2012-09-12 16:43:55 -0400

    test ManifestSignature content

--- test/passifier/test_manifest_signature.rb
+++ test/passifier/test_manifest_signature.rb
@@ -4,6 +4,13 @@ class TestManifestSignature < Test::Unit::TestCase
 
   include Passifier
 
+  def test_content
+    manifest_signature = Helper.new_manifest_signature
+
+    assert_not_nil manifest_signature
+    assert_not_nil manifest_signature.content
+    assert_equal "sign of the timez", manifest_signature.content
+  end
 
 end
 
