71cd2d9244766d061f18e0f855960ed9093b6f5f
Ari Russo <ari.russo@gmail.com>
2012-09-12 17:31:05 -0400

    first pass at storage test

---
+++ test/passifier/test_storage.rb
@@ -0,0 +1,35 @@
+require 'helper'
+
+class TestStorage < Test::Unit::TestCase
+
+  def test_initialize
+  end
+
+  def test_directories
+  end
+
+  def test_path
+  end
+
+  def test_write_file
+  end
+
+  def test_store
+  end
+
+  def test_zip
+  end
+
+  def test_remove_zip
+  end
+
+  def test_directory_exists
+  end
+
+  def test_remove_file
+  end
+
+  def test_remove_scratch_directory
+  end
+
+end
